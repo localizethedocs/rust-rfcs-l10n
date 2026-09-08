27ba0f041089b7f0b628f4456968c33fb8e51702
ltdrobot <ltdrobot@localizethedocs.org>
2026-09-08 13:17:02 +0000

    Deploying to pages from @ localizethedocs/rust-rfcs-l10n@8059433a81d6974f54ec437d1131d65541fa1b08 🚀

---
+++ ltd-config.js
@@ -0,0 +1,19 @@
+"use strict";
+
+var CONFIG_OPTIONS = {
+  CONFIG_LANGUAGES: [
+    ["en-us",     "English"],
+    ["zh-cn",     "简体中文"],
+    ["zh-tw",     "繁體中文"],
+  ],
+  CONFIG_VERSIONS: [
+    ["master",    "Development"],
+  ],
+  CONFIG_PROJECTS: [
+    ["Index",     "https://projects.localizethedocs.org"],
+    ["Crowdin",   "https://localizethedocs.crowdin.com/rust-rfcs-l10n"],
+    ["GitHub",    "https://github.com/localizethedocs/rust-rfcs-l10n"],
+    ["AtomGit",   "https://atomgit.com/localizethedocs/rust-rfcs-l10n"],
+    ["GitFlic",   "https://gitflic.ru/project/localizethedocs/rust-rfcs-l10n"],
+  ]
+};
